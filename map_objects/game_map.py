0170fdc06b4f684d49a10e3a56bacfdbb1a8f2cd
Baltikka <manborov@gmail.com>
2021-09-06 10:06:12 +0700

    tweaks

--- map_objects/game_map.py
+++ map_objects/game_map.py
@@ -105,17 +105,17 @@ class GameMap:
             y = randint(room.y1 + 1, room.y2 - 1)
 
             if not any([entity for entity in entities if entity.x == x and entity.y == y]):
-                if randint(0, 100) < 80:
-                    fighter_component = Fighter(hp=10, defense=0, power=3)
+                if randint(0,100) < 90:
+                    fighter_component = Fighter(hp=10, defense=0, power=2)
                     ai_component = BasicMonster()
                     monster = Entity(x, y, 'o', libtcod.desaturated_green, 'Orc', blocks=True,
                                      render_order=RenderOrder.ACTOR, fighter=fighter_component, ai=ai_component)
                 else:
-                    fighter_component = Fighter(hp=16, defense=1, power=4)
+                    fighter_component = Fighter(hp=15, defense=1, power=3)
                     ai_component = BasicMonster()
                     monster = Entity(x, y, 'T', libtcod.darker_green, 'Troll', blocks=True, fighter=fighter_component,
                                      render_order=RenderOrder.ACTOR, ai=ai_component)
-
+                    
                 entities.append(monster)
     
     def is_blocked(self, x, y):
